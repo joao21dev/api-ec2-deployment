e8ccd19dbcc0c49a5b53712c2a6301e8848bfb06
joao21dev <joao21dev@gmail.com>
2024-09-15 07:19:25 -0300

    Refactor to modularize routes and views
    Removed redundant CSS and HTML files and added new route handlers for admin and shop functionalities. Updated package.json to include body-parser and start script for nodemon.

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const path = require('path');
-
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static('public'));
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'views', 'welcome.html');
-  res.sendFile(filePath);
+app.use(bodyParser.urlencoded());
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
-app.listen(80);
+const port = 80;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
